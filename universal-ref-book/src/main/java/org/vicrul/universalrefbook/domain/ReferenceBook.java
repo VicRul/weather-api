84a93f02542b81c1fc602a189233dac93ff8bdfe
victor.sotskov <vic.sotskov@gmail.com>
2020-11-21 18:30:07 +0700

    Create project

---
+++ universal-ref-book/src/main/java/org/vicrul/universalrefbook/domain/ReferenceBook.java
@@ -0,0 +1,25 @@
+package org.vicrul.universalrefbook.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "reference_book")
+@Data
+@NoArgsConstructor
+public class ReferenceBook {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@NonNull
+	private String name;
+	
+}
